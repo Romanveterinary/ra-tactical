6313c82c73752bb218d6d006a6db9434ba65a898
Romanveterinary <romanshutyak@gmail.com>
2026-06-01 19:22:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ra-tactical-v10.38';
+const CACHE_NAME = 'ra-tactical-v10.40';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -15,7 +15,7 @@ self.addEventListener('install', (event) => {
     self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('[SW] Збереження тактичного пакету v10.38');
+            console.log('[SW] Збереження тактичного пакету v10.40');
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
@@ -39,8 +39,13 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     const req = event.request;
 
-    // Кешування зовнішніх карт (додано mt0.google.com) та мізків ШІ на льоту
-    if (req.url.includes('opentopomap.org') || req.url.includes('cartocdn.com') || req.url.includes('storage.googleapis.com') || req.url.includes('mt0.google.com')) {
+    // Кешування зовнішніх карт (OSM, Topo, Sat, Dark) та ШІ на льоту
+    if (req.url.includes('tile.openstreetmap.org') || 
+        req.url.includes('opentopomap.org') || 
+        req.url.includes('cartocdn.com') || 
+        req.url.includes('storage.googleapis.com') || 
+        req.url.includes('mt0.google.com')) {
+        
         event.respondWith(
             caches.match(req).then((cachedResponse) => {
                 if (cachedResponse) return cachedResponse; 
